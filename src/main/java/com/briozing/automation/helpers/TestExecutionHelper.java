fe0b435d232e514f643cc9e6c0247304a200d237
Sanketkhanorkar <sanket.khanorkar@briozing.com>
2020-06-11 20:26:21 +0530

    added 4th test

--- src/main/java/com/briozing/automation/helpers/TestExecutionHelper.java
+++ src/main/java/com/briozing/automation/helpers/TestExecutionHelper.java
@@ -16,7 +16,7 @@ public class TestExecutionHelper {
     ValidationHelper validationHelper = new ValidationHelper();
     CommonMethods commonMethods = new CommonMethods();
 
-    public void executeTest(Map<String,Boolean> testSteps, HomePage homePage){
+    public void executeTest1(Map<String,Boolean> testSteps, HomePage homePage){
         if(testSteps.get(TestSteps.STEP_LAUNCH_HOME_PAGE.name())!=null && testSteps.get(TestSteps.STEP_LAUNCH_HOME_PAGE.name())){
             try{
                 homePage.launch();
@@ -291,6 +291,127 @@ public class TestExecutionHelper {
 
         if(testSteps.get(TestSteps.STEP_CLICK_MINIMUM_PAYMENT_PLAN_YES.name())!=null && testSteps.get(TestSteps.STEP_CLICK_MINIMUM_PAYMENT_PLAN_YES.name())){
             homePage.minimumMonthlyPaymentYes.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_PLAN_AGREED_MAKE_PAYMENT_CARD_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_PLAN_AGREED_MAKE_PAYMENT_CARD_DISPLAY.name())){
+            validationHelper.planAgreedMakePaymentCardDisplay(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_CLICK_PLAN_AGREED_MAKE_PAYMENT_YES.name())!=null && testSteps.get(TestSteps.STEP_CLICK_PLAN_AGREED_MAKE_PAYMENT_YES.name())){
+            homePage.planAgreedMakePaymentYes.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_PAYMENT_MODE_MESSAGE_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_PAYMENT_MODE_MESSAGE_DISPLAY.name())){
+            validationHelper.validatePaymentModeMessageDisplayed(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_CLICK_CREDIT_CARD_BUTTON.name())!=null && testSteps.get(TestSteps.STEP_CLICK_CREDIT_CARD_BUTTON.name())){
+            homePage.creditCardButton.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_PAYMENT_DETAILS_FORM.name())!=null && testSteps.get(TestSteps.VALIDATE_PAYMENT_DETAILS_FORM.name())){
+            validationHelper.validatePaymentDetailsFromDisplayed(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_FILL_PAYMENT_DETAILS_AND_CLICK_PAY.name())!=null && testSteps.get(TestSteps.STEP_FILL_PAYMENT_DETAILS_AND_CLICK_PAY.name())){
+            validationHelper.fillPaymentDetailsAndPay(homePage);
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_PAYMENT_CONFIRMATION_POPUP_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_PAYMENT_CONFIRMATION_POPUP_DISPLAY.name())){
+            validationHelper.validatePaymentConfirmationPopupDisplay(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_CLICK_CONFIRM_PAYMENT.name())!=null && testSteps.get(TestSteps.STEP_CLICK_CONFIRM_PAYMENT.name())){
+            homePage.confirmPaymentButton.click();
+            commonMethods.pause(2000);
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_PAYMENT_DONE_AND_HELP_CARD.name())!=null && testSteps.get(TestSteps.VALIDATE_PAYMENT_DONE_AND_HELP_CARD.name())){
+            validationHelper.planPaymentDoneAndHelpCardDisplay(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_CLICK_PAYMENT_DONE_AND_HELP_NO.name())!=null && testSteps.get(TestSteps.STEP_CLICK_PAYMENT_DONE_AND_HELP_NO.name())){
+            homePage.planPaymentDoneHelpNo.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_THANK_YOU_MESSAGE.name())!=null && testSteps.get(TestSteps.VALIDATE_THANK_YOU_MESSAGE.name())){
+            validationHelper.validateThankyouMessageDisplay(homePage);
+        }
+    }
+
+    public void executeTest4(Map<String,Boolean> testSteps, HomePage homePage) {
+        if(testSteps.get(TestSteps.STEP_LAUNCH_HOME_PAGE.name())!=null && testSteps.get(TestSteps.STEP_LAUNCH_HOME_PAGE.name())){
+            try{
+                homePage.launch();
+                commonMethods.pause(2000);
+            }catch (Exception ex){
+                logger.error(ex.getMessage());
+                AppAssert.assertTrue(false,ex.getMessage());
+            }
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_ASK_ASPEN_BUTTON_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_ASK_ASPEN_BUTTON_DISPLAY.name())){
+            validationHelper.validateAskAspenButtonDisplayed(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_CLICK_ASK_ASPEN_BUTTON.name())!=null && testSteps.get(TestSteps.STEP_CLICK_ASK_ASPEN_BUTTON.name())){
+            homePage.askAspenButton.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_CHECK_BALANCE_BUTTON_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_CHECK_BALANCE_BUTTON_DISPLAY.name())){
+            validationHelper.validateCheckBalanceButtonDisplayed(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_CLICK_CHECK_BALANCE_BUTTON.name())!=null && testSteps.get(TestSteps.STEP_CLICK_CHECK_BALANCE_BUTTON.name())){
+            homePage.checkBalanceButton.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_LOGIN_FORM_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_LOGIN_FORM_DISPLAY.name())){
+            validationHelper.validateLoginFormDisplayed(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_FILL_CREDENTIALS_AND_LOGIN.name())!=null && testSteps.get(TestSteps.STEP_FILL_CREDENTIALS_AND_LOGIN.name())){
+            validationHelper.fillDetailsAndLogin(homePage);
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_TOTAL_OUTSTANDING_BALANCE_MESSAGE_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_TOTAL_OUTSTANDING_BALANCE_MESSAGE_DISPLAY.name())){
+            validationHelper.validateTotalOutstandingBalanceMessageDisplayed(homePage);
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_PAY_IN_FULL_OPTIONS.name())!=null && testSteps.get(TestSteps.VALIDATE_PAY_IN_FULL_OPTIONS.name())){
+            validationHelper.validatePayInFullOptionButtonsDisplayed(homePage);
+        }
+
+        if (testSteps.get(TestSteps.STEP_CLICK_PAY_IN_FULL_NO.name()) != null && testSteps.get(TestSteps.STEP_CLICK_PAY_IN_FULL_NO.name())) {
+            homePage.payInFullNo.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_SETUP_PAYMENT_PLAN_TODAY_CARD_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_SETUP_PAYMENT_PLAN_TODAY_CARD_DISPLAY.name())){
+            validationHelper.validateSetupPaymentPlanTodayCardDisplay(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_CLICK_SETUP_PAYMENT_PLAN_TODAY_YES.name())!=null && testSteps.get(TestSteps.STEP_CLICK_SETUP_PAYMENT_PLAN_TODAY_YES.name())){
+            homePage.setupPaymentPlanTodayYes.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_RECOMMENDED_PLAN_CARD_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_RECOMMENDED_PLAN_CARD_DISPLAY.name())){
+            validationHelper.validateRecommendedPlanCardDisplay(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_CLICK_RECOMMENDED_PLAN_NO.name())!=null && testSteps.get(TestSteps.STEP_CLICK_RECOMMENDED_PLAN_NO.name())){
+            homePage.recommendedPlanNo.click();
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_ENTER_EMI_MESSAGE.name())!=null && testSteps.get(TestSteps.VALIDATE_ENTER_EMI_MESSAGE.name())){
+            validationHelper.validateEnterEMIMessageDisplay(homePage);
+        }
+
+        if(testSteps.get(TestSteps.VALIDATE_START_CONVERSATION_TEXTBOX.name())!=null && testSteps.get(TestSteps.VALIDATE_START_CONVERSATION_TEXTBOX.name())){
+            validationHelper.validateStartConversation(homePage);
+        }
+
+        if(testSteps.get(TestSteps.STEP_ENTER_AMOUNT_ABOVE_110.name())!=null && testSteps.get(TestSteps.STEP_ENTER_AMOUNT_ABOVE_110.name())){
+            homePage.startConversationInput.sendKeys("120" + Keys.ENTER);
         }
 
         if(testSteps.get(TestSteps.VALIDATE_PLAN_AGREED_MAKE_PAYMENT_CARD_DISPLAY.name())!=null && testSteps.get(TestSteps.VALIDATE_PLAN_AGREED_MAKE_PAYMENT_CARD_DISPLAY.name())){
--- src/test/java/com/briozing/automation/suites/CheckBalanceTest.java
+++ src/test/java/com/briozing/automation/suites/CheckBalanceTest.java
@@ -43,7 +43,7 @@ public class CheckBalanceTest extends BaseTest {
             testSteps.put(TestSteps.VALIDATE_PAYMENT_CONFIRMATION_POPUP_DISPLAY.name(),true);
             testSteps.put(TestSteps.STEP_CLICK_CONFIRM_PAYMENT.name(),true);
             testSteps.put(TestSteps.VALIDATE_PAYMENT_DONE_CONFIRMATION_MESSAGE.name(),true);
-            testExecutionHelper.executeTest(testSteps,homePageObj);
+            testExecutionHelper.executeTest1(testSteps,homePageObj);
         } catch (Exception ex) {
             logger.error(ex.getMessage());
             AppAssert.assertTrue(false, ex.getMessage());
@@ -123,6 +123,46 @@ public class CheckBalanceTest extends BaseTest {
             testSteps.put(TestSteps.STEP_CLICK_PAYMENT_DONE_AND_HELP_NO.name(),true);
             testSteps.put(TestSteps.VALIDATE_THANK_YOU_MESSAGE.name(),true);
             testExecutionHelper.executeTest3(testSteps,homePageObj);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+            AppAssert.assertTrue(false, ex.getMessage());
+        }
+    }
+
+    @Test(groups = {TestConstants.TEST_GROUP_SANITY, "CheckBalance4"}, description = "Verify check balance - pay in full no, setup payment plan yes, recommended plan no, enter amount above 110, make payment yes,select credit card, payment done , thank you")
+    public void verify_check_balance_4() {
+        try {
+            HomePage homePageObj = new HomePage();
+            Map<String,Boolean> testSteps= new HashMap();
+            testSteps.put(TestSteps.STEP_LAUNCH_HOME_PAGE.name(),true);
+            testSteps.put(TestSteps.VALIDATE_ASK_ASPEN_BUTTON_DISPLAY.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_ASK_ASPEN_BUTTON.name(),true);
+            testSteps.put(TestSteps.VALIDATE_CHECK_BALANCE_BUTTON_DISPLAY.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_CHECK_BALANCE_BUTTON.name(),true);
+            testSteps.put(TestSteps.VALIDATE_LOGIN_FORM_DISPLAY.name(),true);
+            testSteps.put(TestSteps.STEP_FILL_CREDENTIALS_AND_LOGIN.name(),true);
+            testSteps.put(TestSteps.VALIDATE_TOTAL_OUTSTANDING_BALANCE_MESSAGE_DISPLAY.name(),true);
+            testSteps.put(TestSteps.VALIDATE_PAY_IN_FULL_OPTIONS.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_PAY_IN_FULL_NO.name(),true);
+            testSteps.put(TestSteps.VALIDATE_SETUP_PAYMENT_PLAN_TODAY_CARD_DISPLAY.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_SETUP_PAYMENT_PLAN_TODAY_YES.name(),true);
+            testSteps.put(TestSteps.VALIDATE_RECOMMENDED_PLAN_CARD_DISPLAY.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_RECOMMENDED_PLAN_NO.name(),true);
+            testSteps.put(TestSteps.VALIDATE_ENTER_EMI_MESSAGE.name(),true);
+            testSteps.put(TestSteps.VALIDATE_START_CONVERSATION_TEXTBOX.name(),true);
+            testSteps.put(TestSteps.STEP_ENTER_AMOUNT_ABOVE_110.name(),true);
+            testSteps.put(TestSteps.VALIDATE_PLAN_AGREED_MAKE_PAYMENT_CARD_DISPLAY.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_PLAN_AGREED_MAKE_PAYMENT_YES.name(),true);
+            testSteps.put(TestSteps.VALIDATE_PAYMENT_MODE_MESSAGE_DISPLAY.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_CREDIT_CARD_BUTTON.name(),true);
+            testSteps.put(TestSteps.VALIDATE_PAYMENT_DETAILS_FORM.name(),true);
+            testSteps.put(TestSteps.STEP_FILL_PAYMENT_DETAILS_AND_CLICK_PAY.name(),true);
+            testSteps.put(TestSteps.VALIDATE_PAYMENT_CONFIRMATION_POPUP_DISPLAY.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_CONFIRM_PAYMENT.name(),true);
+            testSteps.put(TestSteps.VALIDATE_PAYMENT_DONE_AND_HELP_CARD.name(),true);
+            testSteps.put(TestSteps.STEP_CLICK_PAYMENT_DONE_AND_HELP_NO.name(),true);
+            testSteps.put(TestSteps.VALIDATE_THANK_YOU_MESSAGE.name(),true);
+            testExecutionHelper.executeTest4(testSteps,homePageObj);
         } catch (Exception ex) {
             logger.error(ex.getMessage());
             AppAssert.assertTrue(false, ex.getMessage());
